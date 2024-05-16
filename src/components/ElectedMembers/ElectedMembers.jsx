a8bd4d0e01dbef27be5a748992896b7d1e7dbe9f
anityam <89035703+anityam@users.noreply.github.com>
2024-05-16 16:50:46 -0400

    added layout

--- src/components/ElectedMembers/ElectedMembers.jsx
+++ src/components/ElectedMembers/ElectedMembers.jsx
@@ -4,12 +4,11 @@ import Image from 'next/image';
 const ElectedMembers = () => {
     return(
         <div class=" bg-white flex flex-col lg:w-2/3 sm:p-1 p-10 mx-auto ">
-            <thead class="shadow-lg rounded-2xl">
                 <p class = "flex font-bold lg:text-2xl justify-center">2024-2025 Elected Members</p>
                 <p class = "flex font-bold lg:text-2xl justify-center">२०२४ - २०२५ कार्यसमिति सदस्य</p>
-                <ul class="font-normal text-base overflow-auto">
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
+                <div class="grid grid-cols-4 gap-3">
+                    <div class="col-span-4 flex flex-col p-4 mx-auto ">
+                        <Image class="mx-1 px-5 justify-center "
                             src="/Members/RajanGouli.png"
                             width={150}
                             height={150}
@@ -19,7 +18,8 @@ const ElectedMembers = () => {
                                 <li class="text-sky-700"> Kul Prasad Gouli (Rajan) <p>कुल प्रसाद गौली (राजन)</p></li> 
 
                     </div>
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    <div class="col-span-4 flex flex-row justify-center">
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/SabitraSiwakoti.png"
                             width={150}
@@ -29,7 +29,7 @@ const ElectedMembers = () => {
                             <li class="font-bold text-rose-900 whitespace-pre">2. Senior Vice President: <p>(बरिस्ट उपाध्यक्ष):</p></li>
                             <li class="text-sky-700">Sabitra Siwakoti <p>सावित्रा सिवाकोटी</p></li>  
                     </div>
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/GokulSapkota.png"
                             width={150}
@@ -39,7 +39,7 @@ const ElectedMembers = () => {
                             <li class="font-bold text-rose-900 whitespace-pre">3. Vice President: <p>(उपाध्यक्ष):</p></li>
                             <li class="text-sky-700">Gokul Sapkota <p>गोकुल सापकोटा</p></li>  
                     </div>
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/IndiraSimkhada.png"
                             width={150}
@@ -49,7 +49,8 @@ const ElectedMembers = () => {
                             <li class="font-bold text-rose-900 whitespace-pre">4. Vice President: <p>(उपाध्यक्ष):</p></li>
                             <li class="text-sky-700">Indira Simkhada (Pande) <p>इन्दिरा सिंखडा (पाण्डे)</p></li>  
                     </div>
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    </div>
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/RAJENDRAKUMARKARKI.png"
                             width={150}
@@ -58,7 +59,7 @@ const ElectedMembers = () => {
                             />
                             <li class="font-bold text-rose-900 whitespace-pre">5. General Secretary: <p>(महासचिव):</p></li><li class="text-sky-700">Rajendra Kumar Karki <p>राजेन्द्र कुमार कार्की</p></li>  
                     </div>
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/DeepakAdhikari.png"
                             width={150}
@@ -67,7 +68,7 @@ const ElectedMembers = () => {
                             />
                             <li class="font-bold text-rose-900 whitespace-pre">6. Secretary: <p>(सचिव):</p></li><li class="text-sky-700">Deepak Adhikari <p>दिपक अधिकारी</p></li>  
                     </div> 
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/MohanChettry.png"
                             width={150}
@@ -77,7 +78,7 @@ const ElectedMembers = () => {
                             <li class="font-bold text-rose-900 whitespace-pre">7. Secretary: <p>(सचिव):</p></li>
                             <li class="text-sky-700">Mohan Chettri <p>मोहन क्षेत्री</p></li>  
                     </div>                        
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
+                    <div class=" flex flex-col p-4">
                         <Image class="mx-1 px-5"
                             src="/Members/NirmalThapaliya.png"
                             width={150}
@@ -87,139 +88,9 @@ const ElectedMembers = () => {
                             <li class="font-bold text-rose-900 whitespace-pre">8. Treasurer: <p>(कोषाध्यक्ष):</p></li>
                             <li class="text-sky-700">Nirmal Thapaliya <p>निर्मल थपलिया</p></li>  
                     </div>                        
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/RamKumarRai.png"
-                            width={150}
-                            height={150}
-                            alt="Ram Kumar Rai"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">9. Co-Treasurer: <p>(सह कोषाध्यक्ष):</p></li>
-                            <li class="text-sky-700">Ram Kumar Rai <p>राम कुमार राई</p></li>  
-                    </div>                        
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/AmbikaPhuyal.png"
-                            width={150}
-                            height={150}
-                            alt="Ambika Phuyal"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">10. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Ambika Phuyal<p>अम्बिका फुँयाल</p></li>  
-                    </div>   
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/BIkashMainali.png"
-                            width={150}
-                            height={150}
-                            alt="BIkash Mainali"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">11. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Bikash Mainali<p>विकास मैनाली</p></li>  
-                    </div>   
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/BimalaBhandari.png"
-                            width={150}
-                            height={150}
-                            alt="Bimala Bhandari"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">12. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Bimala Bhandari<p>विमला भण्डारी</p></li>  
-                    </div>   
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/YamRajGiri.png"
-                            width={150}
-                            height={150}
-                            alt="Yam Raj Giri"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">13. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Dr.Yam Raj Giri<p>डा. यम राज गिरी</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/GyanuChand.png"
-                            width={150}
-                            height={150}
-                            alt="Gyanu Chand"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">14. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Gyanu Chand<p>ज्ञानु चन्द</p></li>  
-                    </div>  
+                    
+            </div>
 
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/RaviKhadka.png"
-                            width={150}
-                            height={150}
-                            alt="Ravi Khadka"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">15. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Ravi Khadka<p>रवि खड्का</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/RajKumarTamang.png"
-                            width={150}
-                            height={150}
-                            alt="Raj Kumar Tamang"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">16. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Raj kumar Tamang<p>राज कुमार तामांग</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/RoshniSitaula.png"
-                            width={150}
-                            height={150}
-                            alt="Roshni Sitaula"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">17. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Roshni Sitaula<p>रोश्नी सिटौला</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/SujanRegmi.png"
-                            width={150}
-                            height={150}
-                            alt="Sujan Regmi"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">18. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Sujan Regmi<p>सुजन रेग्मी</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/NamitaPaudel.png"
-                            width={150}
-                            height={150}
-                            alt="Namita Paudel"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">19. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Namita Paudel<p>नमिता पौडेल</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/NirajSapkota.png"
-                            width={150}
-                            height={150}
-                            alt="Niraj Sapkota"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">20. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Niraj Sapkota<p>निरज सापकोटा</p></li>  
-                    </div>  
-                    <div class="max-w-screen-xl flex flex-row mx-auto p-4">
-                        <Image class="mx-1 px-5"
-                            src="/Members/SumanBastola.png"
-                            width={150}
-                            height={150}
-                            alt="Suman Bastola"
-                            />
-                            <li class="font-bold text-rose-900 whitespace-pre">21. Board Of Directors: <p>(कार्यसमिति सदस्य):</p></li>
-                            <li class="text-sky-700">Suman Bastola<p>सुमन बास्तोला</p></li>  
-                    </div>  
-            </ul>
-        </thead>
     </div>
             
     )
